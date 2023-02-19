c8a4f305ced21f2b54d7092e346228716896c5c5
Guadixx <guadadoud@gmail.com>
2023-02-19 20:14:01 +0100

    Proyecto react y rutas protegidas

---
+++ 13.ProtectedRoutesTest/src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react"
+import { UserContext } from "../context/userContext"
+import { Navigate } from "react-router-dom";
+
+
+const ProtectedRoute = ({children}) => {
+    const {user} = useContext(UserContext);
+
+    if(!user){
+        return <Navigate to= "/login" />
+    }
+
+    return children;
+}
+
+export default ProtectedRoute;
